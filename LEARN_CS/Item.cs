72cd96472fa092b8e6c86e04dcde5af0bea426d1
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-12-13 17:45:32 +0900

    2025-12-13 JSON 데이터 표현

---
+++ LEARN_CS/Item.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEARN_CS
+{
+    internal class Item
+    {
+        [JsonProperty("LABEL")]
+        public int LABEL { get; set; } // LABEL은 정수로 사용하겠다
+        [JsonProperty("NAME")]
+        public string NAME { get; set; }
+        [JsonProperty("STAT")]
+        public string STAT { get; set; }
+        [JsonProperty("VALUE")]
+        public double VALUE { get; set; }
+        [JsonProperty("PRICE ")]
+        public int PRICE { get; set; }
+    }
+}
---
+++ LEARN_CS/Program.cs
@@ -0,0 +1,29 @@
+﻿using LEARN_CS;
+using Newtonsoft.Json;
+
+// (1) 폴더에서 파일 경로 가져오기
+string folderpath = "C:\\Users\\Administrator\\Desktop\\25CSharp\\LEARN_CS";
+string filename = "Item.json";
+
+string fullpath = Path.Combine(folderpath, filename);
+
+Console.WriteLine(fullpath);
+
+// (2) 파일을 읽어오기
+string text = File.ReadAllText(fullpath);
+Console.WriteLine(text);
+
+// (3) 파일을 데이터로 해석하기 => class 만들기
+// 10개. 1000원. 어떤 아이템을 사겠다.
+// 시작 아이템을 랜덤으로 주는 기능 구현.
+
+// json파일을 클래스로 변환해줘. Nuget- Newtonsoft 다운받은 것
+var ItemDB = JsonConvert.DeserializeObject<List<Item>>(text);
+
+Item select = ItemDB[1];
+
+Console.WriteLine($"LABEL : {select.LABEL}," +
+    $" Name : {select.NAME}, " +
+    $" Stat : {select.STAT}, " +
+    $" Value : {select.VALUE}"); 
+
